2eff1bb1f50d4f2540d974a0d4a64e7ccec090dc
LIFE CODING WITH JAVA <127834681+6206472735rahul@users.noreply.github.com>
2025-04-18 15:06:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 function stringChop(str, size) {
   // your code here
 	let ans=[];
-	for(int i=0;i<str.length;i=i+size){
+	for(let i=0;i<str.length;i=i+size){
 		let subs= str.slice(i,i+size);
 		ans.push(subs);
 	}
 	return ans;
 	
 }
-
 // Do not change the code below
 const str = prompt("Enter String.");
 const size = prompt("Enter Chunk Size.");
