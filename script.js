18fd17bc8e494f1a8e20107008e6f6f6dab54410
LIFE CODING WITH JAVA <127834681+6206472735rahul@users.noreply.github.com>
2025-04-18 14:47:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function stringChop(str, size) {
   // your code here
+	let ans=[];
+	for(int i=0;i<str.length;i+size){
+		let subs= str.slice(i,i+size);
+		ans.push(subs);
+	}
+	return ans;
+	
 }
 
 // Do not change the code below
